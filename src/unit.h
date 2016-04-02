b01a3f650df47e823413c19941f9807c2611359a
makssobolevs <makssobolevs@gmail.com>
2016-04-02 11:30:18 +0300

    Добавлен базовый класс Unit

---
+++ src/unit.h
@@ -0,0 +1,20 @@
+#ifndef UNIT_H
+#define UNIT_H
+
+
+class Unit
+{
+private:
+    int h;
+    int attack;
+    int armor;
+public:
+    Unit(int h, int attack, int armor);
+    void heal();
+    void hit();
+    virtual Do(Unit& unit) = 0;
+    int getHealth();
+
+};
+
+#endif // UNIT_H
