58f18b4ec6a9469a429fe52cca9a8a5aeb336c3d
Lonerzsl <mr.tarazevich@gmail.com>
2016-04-02 10:48:44 +0300

    OOP_example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 using namespace std;
+class Person {
+public:
+	virtual void work()
+	{
+		cout << "Person is working" << endl;
+	}
+	void function(Person& arg)
+	{
+		arg.work();
+	}
+};
+class Student : public Person {
+	void work()
+	{
+		cout << "Student is working" << endl;
+	}
+};
+class Teacher : public Person {
+	void work()
+	{
+		cout << "Teacher is working" << endl;
+	}
+};
 int main()
 {
-	cout << "It is Sevruk Vladislav" << endl;
+	int y = 1024;
+	int* ptr = &y;
+	cout << "BOOOM!" << endl;
+	cout << ptr << "   " << ptr++ << "    " << ptr + 10 << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	Student s;
+	Teacher t;
+	Person p;
+	p.function(s);
+	Person *ps = &s;
+	Person *pt = &t;
+	p.work();
+	ps->work();
+	pt->work();
+
 	return 0;
 }
-
