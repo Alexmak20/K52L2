c1934545cd5c8461c64aeb692c29b7f46767ffbb
Алексей Маковский < >
2016-04-02 12:22:15 +0300

    add unid Alexmak

---
+++ win/K52L2/Alexmak.cpp
@@ -0,0 +1,25 @@
+#include "Alexmak.h"
+
+
+
+Alexmak::Alexmak() : Unit(45,45,10)
+	
+{
+}
+
+
+Alexmak::~Alexmak()
+{
+}
+void Alexmak::Do(Unit& unit)
+{
+	if (getHealth() < 20 )
+	{
+		heal();
+
+	}
+	else
+	{
+		hit();
+	}
+}
