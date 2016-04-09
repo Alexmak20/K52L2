8a7da585df15201bd7c9be3847a555d2171028a5
Gpef <oleg.baslak@gmail.com>
2016-04-09 11:22:29 +0300

    init

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,30 @@ using namespace std;
 int main()
 {
 
+	//
+	// Arena* arena;
+	// arena = new Arena();
+
+
+	std::cout << "Welcome!" << " Choose a menu: /n"
+		<< "1 - Add new Hero to epic Arena /n"
+		<< "2 - Start epic Arena /n"
+		<< "0 - Exit from the depths of hell/n"
+		<< endl;
+
+	int switcher = 0; std::cin >> switcher;
+	switch (switcher)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+	case 0:
+		break;
+	default:
+		break;
+	}
+
+
 	return 0;
 }
